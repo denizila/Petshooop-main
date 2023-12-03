6be985913621bc669cb9dbeece17e4225edcc0e4
Richard-DevZX <gabrielrichard560@gmail.com>
2023-12-03 17:27:20 -0300

    Adicionando loguin.

--- app/controllers/Logins.java
+++ app/controllers/Logins.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.Pessoa;
+import models.Veterinario;
 import play.mvc.Controller;
 
 public class Logins extends Controller {
@@ -12,10 +13,17 @@ public class Logins extends Controller {
 	public static void logar(String email, String senha) {
 
 		Pessoa pessoalog = Pessoa.find("email = ?1 and senha = ?2", email, senha).first();
+		Veterinario doutor = Veterinario.find("emailcomercial = ?1 and senha = ?2", email, senha).first();
 		if (pessoalog != null) {
-			session.put("Usuário Logado", pessoalog.nome);
+			session.put("UsuarioLogado", pessoalog.nome);
+			session.put("usuarioId", pessoalog.id);
 			flash.success("Login realizado com sucesso!");
-			Pessoas.listar();
+			Pets.form();
+		} else if(doutor != null) {
+			session.put("usuarioLogado", doutor.nome);
+			session.put("usuarioId", doutor.id);
+			flash.success("Login realizado com sucesso");
+			Veterinarios.agenda(doutor.id);
 		}
 
 		flash.error("Credenciais inválidas ");
