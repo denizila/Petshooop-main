568342e8b7869bb548e42220743b9c954ac55865
Richard-DevZX <gabrielrichard560@gmail.com>
2023-12-03 10:58:06 -0300

    Teste de commit.

--- app/controllers/Pets.java
+++ app/controllers/Pets.java
@@ -1,38 +1,44 @@
 package controllers;
+
 import java.util.List;
 import models.Pessoa;
 import models.Pet;
 import play.mvc.Controller;
 
-public class Pets extends Controller{
-	 public static void form() {
+public class Pets extends Controller {
+	public static void form() {
 		List<Pessoa> pessoas = Pessoa.findAll();
-		 render(pessoas);
-	  }
-	  public static void cadastrar(Pet m) {
-	  
-	 m.save();
-	  listar();
-	  }
-	  public static void listar() {
-		 String buscar = params.get("buscar");
-		  
-	  	  List<Pet> petsLista;
-	  	  if(buscar == null) {
-	  	 petsLista= Pet.findAll();
-	  	} else { 
-	  		petsLista = Pet.find("lower(nome) like ?1 or lower(especie) like ?1",
-					"%"+ buscar.toLowerCase() +"%").fetch();}
-		 render(petsLista);
-	  }
-	  public static void remover(Long id) {
-		  Pet m = Pet.findById(id);
-		  m.delete();
-		  listar();
-	  }
-	  public static void editar(Long id) {
-		  Pet m = Pet.findById(id);
-		  	List<Pessoa> pessoas = Pessoa.findAll();
-		 renderTemplate("Pets/form.html", m, pessoas);
-	  }
+		render(pessoas);
+	}
+
+	public static void cadastrar(Pet m) {
+
+		m.save();
+		listar();
+	}
+
+	public static void listar() {
+		String buscar = params.get("buscar");
+
+		List<Pet> petsLista;
+		if (buscar == null) {
+			petsLista = Pet.findAll();
+		} else {
+			petsLista = Pet.find("lower(nome) like ?1 or lower(especie) like ?1", "%" + buscar.toLowerCase() + "%")
+					.fetch();
+		}
+		render(petsLista);
+	}
+
+	public static void remover(Long id) {
+		Pet m = Pet.findById(id);
+		m.delete();
+		listar();
+	}
+
+	public static void editar(Long id) {
+		Pet m = Pet.findById(id);
+		List<Pessoa> pessoas = Pessoa.findAll();
+		renderTemplate("Pets/form.html", m, pessoas);
+	}
 }
